c8286728660d2cfb6e7b8e47435af175e3d995d3
hye <hyebeenbyeon@gmail.com>
2022-11-18 04:05:43 +0900

    add enum QuestLevel

--- 3DCP_FINAL/Assets/Scripts/QuestManager.cs
+++ 3DCP_FINAL/Assets/Scripts/QuestManager.cs
@@ -1,22 +1,42 @@
+// Scriptable Object 설정 -> 어차피 씬이 하나라 상관 없나?
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Quest Data Struct
+public enum QuestLevel 
+{
+    START, FIRST, MID, SECOND
+} 
+
 [System.Serializable]
 public struct QuestData
 {
+    public int index;
+    public QuestLevel level;
     public string desc;
     public bool open;
     public bool clear;
 }
+
 public class QuestManager : MonoBehaviour
 {
     [SerializeField]
-    private List<QuestData> questDatas;
+    List<QuestData> datas;
+    // 현재 실행중인 퀘스트 목록
 
     private void Awake() 
     {
-        
+
     }
+
+    private void Update() 
+    {
+
+    }
+
+    // 퀘스트 UI 활성화
+    // 현재 수행중인 퀘스트 목록에 업데이트
+    // 현재 수행중인 퀘스트 목록
+    // Quest 함수
 }
