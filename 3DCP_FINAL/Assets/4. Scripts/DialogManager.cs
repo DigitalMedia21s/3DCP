00df008e2428cde72d6235f63aff991c94ddbc89
hye <hyebeenbyeon@gmail.com>
2022-12-05 01:46:54 +0900

    update Dialog datas to Json from hierachy

--- 3DCP_FINAL/Assets/4. Scripts/DialogManager.cs
+++ 3DCP_FINAL/Assets/4. Scripts/DialogManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using TMPro;
 using DG.Tweening;
 
@@ -13,21 +14,26 @@ public class DialogManager : MonoBehaviour
     [SerializeField] private Transform parentContent;
     // viewerNicknameText => TMP_Text?
     // viewerDialogText => TMP_Text?
-    [SerializeField] private List<PlayerDialog> playerDialogs;
-    [SerializeField] private List<ViewerDialog> viewerDialogs;
-    [SerializeField] private ViewerDialog loopViewerDialogs;
+    // 댓글 코루틴은 한 개만
+    private DialogDatas datas;
 
+    private void Awake() 
+    {
+        Debug.LogWarning("Awake");
+        
+        Load();
+    }
     private void Start() 
     {
-        ShowDialog("시작");
+        ShowDialog("복도");
     }
     /// <summary> 
     /// 대사를 시작하고 싶은 곳에서 이 함수를 호출하세요. 매개변수로 대사의 ID를 사용합니다. 
     /// </summary>
     public void ShowDialog(string id)
     {
-        PlayerDialog pDialog = playerDialogs.Find(x => x.id == id);
-        ViewerDialog vDialog = viewerDialogs.Find(x => x.id == id);
+        PlayerDialog pDialog = datas.playerDialogs.Find(x => x.id == id);
+        ViewerDialog vDialog = datas.viewerDialogs.Find(x => x.id == id);
 
         if (pDialog == null) Debug.LogWarning("CAN NOT FIND PLAYER DIALOG");
         else StartCoroutine(ShowPlayerDialog(pDialog));
@@ -57,14 +63,26 @@ public class DialogManager : MonoBehaviour
         {
             GameObject clone = Instantiate(viewerTextPrefab, parentContent);
             clone.GetComponent<TextMeshProUGUI>().text = $"{c.name} : {c.content}";
-            yield return new WaitForSeconds(interval*2);
+            yield return new WaitForSeconds(interval);
         }
         // 반복 실행
     }
+    public void Load() 
+    {
+        Debug.LogWarning("Load 시작");
 
+        TextAsset t = Resources.Load<TextAsset>("DialogData");
+        datas = JsonUtility.FromJson<DialogDatas>(t.text);
+        Debug.LogWarning("Complete Data Load");
+    }
 }
 
-
+[System.Serializable]
+public class DialogDatas
+{
+    public List<PlayerDialog> playerDialogs; // => dictionary? can not serialize
+    public List<ViewerDialog> viewerDialogs;
+}
 [System.Serializable]
 public class PlayerDialog
 {
