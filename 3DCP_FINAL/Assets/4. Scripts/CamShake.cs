724e9f04e88d426de57499ebdf724e6dcad613d5
ililililililililililililililil <rladuswn0605@gmail.com>
2022-12-06 00:54:28 +0900

    오브젝트들 흔들기

---
+++ 3DCP_FINAL/Assets/4. Scripts/CamShake.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShake : MonoBehaviour
+{
+    public float shakeTime = 1.0f;
+    public float shakeSpeed = 2.0f;
+    public float shakeAmount = 1.0f;
+
+    Transform cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam = this.transform;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartCoroutine(Shake());
+        }
+    }
+
+    IEnumerator Shake()
+    {
+        Vector3 originPosition = cam.localPosition;
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < shakeTime)
+        {
+            Vector3 randomPoint = originPosition + Random.insideUnitSphere * shakeAmount;
+            cam.localPosition = Vector3.Lerp(cam.localPosition, randomPoint, Time.deltaTime * shakeSpeed);
+
+            yield return null;
+
+            elapsedTime += Time.deltaTime;
+        }
+
+        cam.localPosition = originPosition;
+    }
+
+}
