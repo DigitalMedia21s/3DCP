7edb8ce6bbbe3d1c7791e0dbd8fa3d756f6437a8
ililililililililililililililil <rladuswn0605@gmail.com>
2022-12-06 22:21:39 +0900

    맵수정

--- 3DCP_FINAL/Assets/4. Scripts/ChairShake.cs
+++ 3DCP_FINAL/Assets/4. Scripts/ChairShake.cs
@@ -6,10 +6,11 @@ public class ChairShake : MonoBehaviour
 {
     float rotSpeed = 30f;
 
-    public float shakeTime = 0.5f;
+    public float shakeTime = 0.4f;
 
     private void Start()
     {
+        //this.transform.rotation = Quaternion.Euler(75, 0, 0);
         StartCoroutine(Chair());
     }
 
@@ -30,26 +31,6 @@ public class ChairShake : MonoBehaviour
         while (elapsedTime < shakeTime)
         {
             transform.Rotate(new Vector3(rotSpeed * Time.deltaTime, 0, 0));
-            elapsedTime += Time.deltaTime;
-
-            yield return null;
-        }
-
-        elapsedTime = 0.0f;
-
-        while (elapsedTime < shakeTime)
-        {
-            transform.Rotate(new Vector3(rotSpeed * Time.deltaTime, 0, 0));
-            elapsedTime += Time.deltaTime;
-
-            yield return null;
-        }
-
-        elapsedTime = 0.0f;
-
-        while (elapsedTime < shakeTime)
-        {
-            transform.Rotate(new Vector3(-rotSpeed * Time.deltaTime, 0, 0));
             elapsedTime += Time.deltaTime;
 
             yield return null;
