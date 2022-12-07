f2f255cc6342472d4917a6e8095506aa3ee16a8e
ililililililililililililililil <rladuswn0605@gmail.com>
2022-12-08 05:06:02 +0900

    플레이어

---
+++ 3DCP_FINAL/Assets/4. Scripts/PlayerColi.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColi : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject ghost1;
+    public GameObject ghost2;
+    public GameObject ghost3;
+
+    public GameObject floor;
+    public GameObject hole1;
+    public GameObject hole2;
+    public GameObject hole3;
+    public GameObject hole4;
+    public GameObject hole5;
+    public GameObject hole6;
+    public GameObject hole7;
+    public GameObject hole8;
+    public GameObject hole9;
+    public GameObject hole10;
+    public GameObject hole11;
+    public GameObject hole12;
+    public GameObject hole13;
+    public GameObject hole14;
+    public GameObject hole15;
+    public GameObject hole16;
+    public GameObject hole17;
+    public GameObject hole18;
+    public GameObject hole19;
+    public GameObject hole20;
+
+    public GameObject ghostWall;
+
+    public GameObject gameoverUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Ghost1Coli")
+        {
+            if (ghost1.activeSelf == true)
+            {
+                ghostWall.SetActive(true);
+
+                player.GetComponent<CamShake>().StartShake();
+
+                StartCoroutine(Hole());
+            }
+        }
+
+        if (other.name == "Ghost2Coli")
+        {
+            if (ghost2.activeSelf == true)
+            {
+                ghost2.SetActive(false);
+                ghost3.SetActive(true);
+            }
+        }
+    }
+
+    IEnumerator Hole()
+    {
+        floor.SetActive(false);
+
+        hole1.SetActive(true);
+        hole2.SetActive(true);
+        hole3.SetActive(true);
+        hole4.SetActive(true);
+        hole5.SetActive(true);
+        hole6.SetActive(true);
+        hole7.SetActive(true);
+        hole8.SetActive(true);
+        hole9.SetActive(true);
+        hole10.SetActive(true);
+        hole11.SetActive(true);
+        hole12.SetActive(true);
+        hole13.SetActive(true);
+        hole14.SetActive(true);
+        hole15.SetActive(true);
+        hole16.SetActive(true);
+        hole17.SetActive(true);
+        hole18.SetActive(true);
+        hole19.SetActive(true);
+        hole20.SetActive(true);
+
+        ghost1.SetActive(false);
+        ghost2.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(5.0f);
+
+        hole1.SetActive(false);
+        hole2.SetActive(false);
+        hole3.SetActive(false);
+        hole4.SetActive(false);
+        hole5.SetActive(false);
+        hole6.SetActive(false);
+        hole7.SetActive(false);
+        hole8.SetActive(false);
+        hole9.SetActive(false);
+        hole10.SetActive(false);
+        hole11.SetActive(false);
+        hole12.SetActive(false);
+        hole13.SetActive(false);
+        hole14.SetActive(false);
+        hole15.SetActive(false);
+        hole16.SetActive(false);
+        hole17.SetActive(false);
+        hole18.SetActive(false);
+        hole19.SetActive(false);
+        hole20.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ghost"))
+        {
+            gameoverUI.SetActive(true);
+        }
+    }
+
+    public void Retry()
+    {
+        Debug.Log("¹öÆ°");
+        if (ghost1.activeSelf == true)
+        {
+            Debug.Log("1¹ø ±Í½Å");
+            ghost1.transform.position = new Vector3(-63.71f, 18.71f, -145.6f);
+            this.transform.position = new Vector3(-75.4f, 18f, -152.6f);
+            gameoverUI.SetActive(false);
+        }
+        else if (ghost2.activeSelf == true)
+        {
+            ghost2.transform.position = new Vector3(-111.4f, 18.71f, -102.1f);
+            this.transform.position = new Vector3(-75.4f, 18f, -107.7f);
+            gameoverUI.SetActive(false);
+        }
+        else if (ghost3.activeSelf == true)
+        {
+            ghost3.transform.position = new Vector3(-10.2f, -8f, -44.1f);
+            this.transform.position = new Vector3(-6.18f, -9f, -92.5f);
+            gameoverUI.SetActive(false);
+        }
+    }
+}
